f933b2659da268797d69cb3e0de76ecfd27c90ca
Janis Mozgis <janismozgis@gmail.com>
2017-10-20 15:59:30 +0300

    final commit

--- tests/02_look_for_camera.js
+++ tests/02_look_for_camera.js
@@ -3,6 +3,7 @@ module.exports = {
     browser
       .url('https://amazon.com')
       .searchKeyword('playstation 4 camera')
+      .click('.s-top-nav-all-filter-expand-text')
       .selectCategoryOnTheLeftFirstLevel('PlayStation 4 Accessories')
       .selectShipping('Ship to Lithuania')
       .selectReview(4)
--- tests/03_look_for_game.js
+++ tests/03_look_for_game.js
@@ -3,6 +3,7 @@ module.exports = {
     browser
       .url('https://amazon.com')
       .searchKeyword('playstation 4 just dance')
+      .click('.s-top-nav-all-filter-expand-text')
       .selectCategoryOnTheLeftFirstLevel('PlayStation 4 Games')
       .selectShipping('Ship to Lithuania')
       .selectGenreOnTheLeft('Music & Dance')
