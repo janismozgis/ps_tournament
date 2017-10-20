f7011e42f33ff1f5c6bd38e7a3741f5b22a99362
Janis Mozgis <janismozgis@gmail.com>
2017-10-20 12:32:31 +0300

    first test

--- tests/01_test.js
+++ tests/01_test.js
@@ -2,6 +2,11 @@ module.exports = {
   'test amazon' : function (browser) {
     browser
       .url('https://amazon.com')
+      .waitForElementVisible('#twotabsearchtextbox', 10000)
+      .setValue('#twotabsearchtextbox', 'playstation 4')
+      .waitForElementVisible('#nav-search-submit-text', 10000)
+      .waitForElementVisible('.nav-search-submit.nav-sprite', 10000)
+      .click('.nav-search-submit.nav-sprite')
       .end();
   }
 };
